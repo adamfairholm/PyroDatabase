ff6cd0454d92e0a561261863e1bbef6acf08d1cc
Adam Fairholm <adam.fairholm@gmail.com>
2011-01-29 18:57:11 -0500

    Adding pretty icons.

--- database/views/admin/partials/shortcuts.php
+++ database/views/admin/partials/shortcuts.php
@@ -1,9 +1,9 @@
 <nav id="shortcuts">
 	<ul>
-		<li><a href="<?php echo site_url('admin/database');?>">Stats</a></li>
-		<li><a href="<?php echo site_url('admin/database/tables');?>">Tables</a></li>
-		<li><a href="<?php echo site_url('admin/database/processes');?>">Processes</a></li>
-		<li><a href="<?php echo site_url('admin/database/query');?>">Query</a></li>
+		<li><a class="stats" href="<?php echo site_url('admin/database');?>">Stats</a></li>
+		<li><a class="tables" href="<?php echo site_url('admin/database/tables');?>">Tables</a></li>
+		<li><a class="processes" href="<?php echo site_url('admin/database/processes');?>">Processes</a></li>
+		<li><a class="query" href="<?php echo site_url('admin/database/query');?>">Query</a></li>
 	</ul>
 	<br class="clear-both" />
 </nav>
