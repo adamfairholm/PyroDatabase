22e74ff7ec147f31b8feaa315723aa82138909b7
Christian Giupponi <christian.giupponi@stilogo.it>
2012-12-24 11:05:50 +0100

    Added italian lang
    Added italian lang

---
+++ language/italian/pyrodatabase_lang.php
@@ -0,0 +1,64 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/* Messages */
+
+$lang['pyrodb:must_select_table'] 						= "Devi selezionare almeno una tabella per eseguire questa azione.";
+$lang['pyrodb:no_results'] 								= "Non ci sono stati risultati con questa query.";
+$lang['pyrodb:following_tables'] 						= "Le seguenti tabelle sono state";
+$lang['pyrodb:following_tables_optimized']				= "ottimizzate";
+$lang['pyrodb:following_tables_repaired']				= "riparate";
+
+/* Buttons */
+
+$lang['pyrodb:button_repair']							= "Ripara tabelle";
+$lang['pyrodb:button_optimize']							= "Ottimizza tabelle";
+
+/* Errors */
+
+$lang['pyrodb.invalid_table_name'] 						= "Nome tabella non valido.";
+
+/* Lang */
+
+$lang['pyrodb:database'] 								= "Database";
+$lang['pyrodb:mysql_version']							= "Versione MySQL";
+$lang['pyrodb:mysql_host']								= "MySQL Host";
+$lang['pyrodb:db_encoding']								= "Database Encoding";
+$lang['pyrodb:mysql_protocol']							= "MySQL Protocollo";
+$lang['pyrodb:query_results'] 							= "Risultati query";
+$lang['pyrodb:db_processes']							= "Database Processes";
+$lang['pyrodb:user']									= "User";
+$lang['pyrodb:host']									= "Host";
+$lang['pyrodb:command']									= "Comando";
+$lang['pyrodb:time']									= "Time";
+$lang['pyrodb:state']									= "Stato";
+$lang['pyrodb:info']									= "Info";
+$lang['pyrodb:col_name']								= "Nome colonna";
+$lang['pyrodb:col_type']								= "Tipo colonna";
+$lang['pyrodb:constraint']								= "Constraint";
+$lang['pyrodb:notes']									= "Note";
+$lang['pyrodb:primary_key']								= "Chiave primaria";
+$lang['pyrodb:db_tables']								= "Tabelle Database";
+$lang['pyrodb:table_name']								= "Nome tabella";
+$lang['pyrodb:engine']									= "Engine";
+$lang['pyrodb:rows']									= "Righe";
+$lang['pyrodb:size']									= "Dimensione";
+$lang['pyrodb:comment']									= "Commento";
+$lang['pyrodb:stats']									= "Statistiche";
+$lang['pyrodb:tables']									= "Tabelle";
+$lang['pyrodb:processes']								= "Processi";
+$lang['pyrodb:query']									= "Query";
+$lang['pyrodb:optimize']								= "Ottimizza";
+$lang['pyrodb:repair']									= "Ripara";
+$lang['pyrodb:optimize_repair']							= "Ottimizza/Ripara";
+$lang['pyrodb:export']									= "Esporta";
+$lang['pyrodb:failure']									= "Fallimanto";
+$lang['pyrodb:file_format']								= "Formato File";
+$lang['pyrodb:filename']								= "Nome del file";
+$lang['pyrodb:newline']									= "Nuova line";
+$lang['pyrodb:run_query']								= "Esegui Query";
+
+$lang['pyrodb:include_drop']	= "Includi la dicitura DROP TABLE";
+$lang['pyrodb:include_insert']	= "Includi la dicitura INSERT";
+$lang['pryodb:filename_instructions'] = "Necessario solo se vuoi esportare in zip.";
+
+/* End of file pyrodatabase_lang.php */
